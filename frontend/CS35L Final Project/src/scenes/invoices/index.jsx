a6a826c633df73ccbcf19a9a4d0f6d769aeb24f9
willhuang04 <160533576+willhuang04@users.noreply.github.com>
2024-03-01 21:08:27 -0800

    Add files via upload

---
+++ frontend/CS35L Final Project/src/scenes/invoices/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Invoices = () => {
+    return <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Header title="Latest Auctions" subtitle="Recommended classes listed below"/>
+        </Box>
+    </Box>
+}
+
+export default Invoices;
