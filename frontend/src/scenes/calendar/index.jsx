0c671fab31d3a7ac277de610cd29c0c0da373b2e
Julia Wang <julialulu.wang@gmail.com>
2024-03-05 13:43:39 -0800

    test for adding classes to calendar

--- frontend/src/scenes/calendar/index.jsx
+++ frontend/src/scenes/calendar/index.jsx
@@ -1,32 +1,53 @@
 import {Box} from "@mui/material";
 import Header from "../../components/Header";
-import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
 import { useState, useRef, useEffect } from 'react';
+
+//must do: npm install @daypilot/daypilot-lite-react 
+import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react"; 
+
+
+//mock data for scheduled classes: array of objects
+const myClasses = [
+    {
+        //replace with some way to get the data in this format ???
+        id: 1,
+        text: "MATH 100",
+        resource: "mon",
+        start: "2024-03-05T09:30:00",
+        end: "2024-03-05T10:45:00",
+        //prof:
+        //location:
+        barColor: "#fcb711",
+    },
+    {
+        id: 2,
+        text: "COM SCI 100",
+        resource: "wed",
+        start: "2024-03-05T14:00:00",
+        end: "2024-03-05T16:50:00",
+        //prof:
+        //location:
+        barColor: "#f37021",
+    },
+
+];
 
 
 const Calendar = () => {
     
     // create ref using useRef hook
-    const calendarRef = useRef();
+    const calendarRef = useRef(null);
     // define function to access DayPilot.Calendar methods
+    // This function acts as a getter for the calendar control
     const getCalendar = () => {
-        return calendarRef.current.control;
-    }
+    return calendarRef.current ? calendarRef.current.control : null;
+    };
     
 
-
-    // State for viewType
+    // State for config, set default viewType and hours to display 
     const [config, setConfig] = useState({
         viewType: "Resources",
         
-        columns: [
-            { name: "Monday", id: "mon" },
-            { name: "Tuesday", id: "tues" },
-            { name: "Wednesday", id: "wed" },
-            { name: "Thursday", id: "thurs" },
-            { name: "Friday", id: "fri" },
-        ],
-
         //businessBeginsHour: 8, //default start at 8 AM
         //businessEndsHour: 21, //default end at 9 PM
         
@@ -37,6 +58,26 @@ const Calendar = () => {
     // State for startDate
     const [startDate, setStartDate] = useState(DayPilot.Date.today());
 
+    // State for columns (load during initial render)
+    const [columns, setColumns] = useState([]);
+    // State for calendar events (selected courses to be displayed)
+    const [scheduledCourses, setScheduledCourses] = useState([]);
+
+    //initialize columns and events (classes) with useEffect
+    useEffect(() => {
+        setColumns([
+            { name: "Monday", id: "mon" },
+            { name: "Tuesday", id: "tues" },
+            { name: "Wednesday", id: "wed" },
+            { name: "Thursday", id: "thurs" },
+            { name: "Friday", id: "fri" },
+        ]);
+        setScheduledCourses(myClasses);
+
+    }, []);
+
+
+
     return (
         
         <Box>
@@ -45,9 +86,14 @@ const Calendar = () => {
             
             {...config}
             startDate={startDate}
+            columns={columns}
+            events={scheduledCourses}
             ref={calendarRef}
 
             />
+
+            <Header subtitle="Search for Classes and Add to Plan"/>
+
         </Box>
         
 
