12c0b7871524a9994a61a8b8146ddedb27b944a7
daowilliam310 <daowilliam310@gmail.com>
2024-03-08 02:52:57 -0800

    Changed fetch statements so that we don't have to be on port 4000 to view auction info on dashboard. Changed ProtectedRoute implementation so that upon refresh, one is not directed to login screen despite already being logged in.

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,15 +1,16 @@
-import { Navigate } from 'react-router-dom';
-
-// Assuming you have a way to check authentication status (e.g., from context or local storage)
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';  
 
 function ProtectedRoute({ children }) {
-  const { user } = useAuthContext(); // Replace this with your actual authentication logic
+  const location = useLocation(); 
   
+ 
+  const user = useAuthContext(); 
+  
+ 
   if (!user) {
-    // User not logged in, redirect to login page, but store which part of the site user was on so they can go back to it 
-    //immediately upon logging in 
-    return <Navigate to="/login" replace />;
+   
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
   
   return children;
